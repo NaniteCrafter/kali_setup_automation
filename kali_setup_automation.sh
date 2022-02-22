d6a668889f1b04b918d311e741b5a1c8b1b83d82
NaniteCrafter <a.sky.flooded.with.light@gmail.com>
2022-02-22 15:40:21 +1100

    update

--- kali_setup_automation.sh
+++ kali_setup_automation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install terminator
-sudo apt-get install cherrytree
-sudo apt-get install gobuster
-
+sudo apt-get install terminator -y
+sudo apt-get install cherrytree -y
+sudo apt-get install gobuster -y
+sudo apt-get install seclists -y
 
 
 sudo apt-get autoclean && sudo apt-get clean && sudo apt-get autoremove
